9b13150aceb96c32cc4950788104824e603d198c
hjh0858-staby <hjh0858@staby.co.kr>
2022-08-31 09:39:50 +0900

    Initial Commit

---
+++ WalletHwang/WalletHwangApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletHwangApp.swift
+//  WalletHwang
+//
+//  Created by 황재현 on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct WalletHwangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
